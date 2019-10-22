82df02852bdada7b5951600709370dc60b796d59
Mor Tsadok <Mortmanage@gmail.com>
2019-10-22 21:30:38 +0300

    initial commit

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 import "./Notifications.css";
+import Toast from "react-bootstrap/Toast";
 
 class Notifications extends Component {
+  state = {
+    toast: true
+  };
+  onClick = () => {
+    console.log(this.state.toast);
+    this.setState({
+      toast: false
+    });
+  };
   render() {
     return (
       <div className="Notifications">
@@ -11,6 +21,17 @@ class Notifications extends Component {
   }
 }
 
-const NotificationsBox = props => <div className="NotificationsBox"></div>;
+const NotificationsBox = props => (
+  <div className="NotificationsBox">
+    <Toast>
+      <Toast.Header>
+        <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+        <strong className="mr-auto">מור צדוק</strong>
+        <small>לפני 11 דקות</small>
+      </Toast.Header>
+      <Toast.Body>.הודעה חדשה ממנהל חדר הכושר</Toast.Body>
+    </Toast>
+  </div>
+);
 
 export default Notifications;
