b750ccac174e38691a781af38e2d779d853bbf7c
Mor Tsadok <Mortmanage@gmail.com>
2019-10-29 21:44:20 +0200

    initial commit

--- src/components/Myprofile/Myprofile.js
+++ src/components/Myprofile/Myprofile.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import "../../css/Myprofile.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDumbbell,
+  faPeopleCarry,
+  faCalendarCheck,
+  faListUl,
+  faHeartbeat,
+  faCog
+} from "@fortawesome/free-solid-svg-icons";
 
 class Myprofile extends Component {
   render() {
@@ -15,11 +24,17 @@ class Myprofile extends Component {
 const SecondNAV = props => (
   <div className="secNAV">
     <div className="Att">
+      <FontAwesomeIcon className="muscleIcon" icon={faDumbbell} />
       <NavLink to="/Processing"> מדדים </NavLink>
+      <FontAwesomeIcon className="HandsHelping" icon={faPeopleCarry} />
       <NavLink to="/Personaltraining">אימון אישי</NavLink>
+      <FontAwesomeIcon className="CalendarCheck" icon={faCalendarCheck} />
       <NavLink to="/Subscription"> חידוש מנוי </NavLink>
+      <FontAwesomeIcon className="ListUL" icon={faListUl} />
       <NavLink to="/TrainingPlan"> תוכנית אימונים </NavLink>
+      <FontAwesomeIcon className="Heartbeat" icon={faHeartbeat} />
       <NavLink to="/Health">הצהרת בריאות</NavLink>
+      <FontAwesomeIcon className="Settings" icon={faCog} />
       <NavLink to="/UserSettings">הגדרות</NavLink>
     </div>
   </div>
--- src/components/Myprofile/UserSettings/UserSettings.js
+++ src/components/Myprofile/UserSettings/UserSettings.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import "../../../css/Myprofile.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDumbbell,
+  faPeopleCarry,
+  faCalendarCheck,
+  faListUl,
+  faHeartbeat,
+  faCog
+} from "@fortawesome/free-solid-svg-icons";
 
 class UserSettings extends Component {
   render() {
@@ -17,11 +26,17 @@ const UnderNAV = props => <div className="underNAV"></div>;
 const SecondNAV = props => (
   <div className="secNAV">
     <div className="Att">
+      <FontAwesomeIcon className="muscleIcon" icon={faDumbbell} />
       <NavLink to="/Processing"> מדדים </NavLink>
+      <FontAwesomeIcon className="HandsHelping" icon={faPeopleCarry} />
       <NavLink to="/Personaltraining">אימון אישי</NavLink>
+      <FontAwesomeIcon className="CalendarCheck" icon={faCalendarCheck} />
       <NavLink to="/Subscription"> חידוש מנוי </NavLink>
+      <FontAwesomeIcon className="ListUL" icon={faListUl} />
       <NavLink to="/TrainingPlan"> תוכנית אימונים </NavLink>
+      <FontAwesomeIcon className="Heartbeat" icon={faHeartbeat} />
       <NavLink to="/Health">הצהרת בריאות</NavLink>
+      <FontAwesomeIcon className="Settings" icon={faCog} />
       <NavLink to="/UserSettings">הגדרות</NavLink>
     </div>
   </div>
