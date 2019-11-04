8132be10f8914fa13e89080c16b62121fdc3895a
Mor Tsadok <Mortmanage@gmail.com>
2019-11-04 21:59:21 +0200

    initial commit

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import { Carousel } from "react-bootstrap";
 import "./Store.css";
+import pic1 from "../../img/pic1.jpg";
+import pic2 from "../../img/pic2.jpg";
+import pic3 from "../../img/pic3.jpg";
 
 class Store extends Component {
   render() {
@@ -13,7 +17,54 @@ class Store extends Component {
 
 const BoxStore = props => (
   <div className="boxStore">
-    <div className="Inside_BoxStore"></div>
+    <div className="Inside_BoxStore">
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={pic1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={pic2} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={pic3} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={pic1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={pic2} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={pic3} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   </div>
 );
 
