1ad7a734ecdfe7578f1d3548e060616b19dddf8f
Mor Tsadok <Mortmanage@gmail.com>
2019-11-10 17:07:37 +0200

    initial commit

--- src/components/Live/Live.js
+++ src/components/Live/Live.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Spinner from "react-bootstrap/Spinner";
 import "./Live.css";
 
 class Live extends Component {
@@ -16,11 +17,26 @@ const LiveCenter = props => (
     <div className="Inside_LiveStore">
       <p class="font-weight-light">מתאמנים בזמן אמת</p>
       <div className="Time-content">
-        <div className="Time">11:45</div>
-        <div className="Inside">
-          <h1 className="countExmple">23</h1>
+        <div className="Time2">
+          <div className="Time1">
+            <div className="Time">11:45</div>
+          </div>
         </div>
-        <div className="Date">9/11/19</div>
+        <div className="SpinnerClass">
+          <Spinner animation="grow" size="la" />
+          <div className="Inside2">
+            <div className="Inside1">
+              <div className="Inside">
+                <h1 className="countExmple">23</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Date2">
+          <div className="Date1">
+            <div className="Date">9/11/19</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
