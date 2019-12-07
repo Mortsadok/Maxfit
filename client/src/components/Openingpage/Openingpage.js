e4bebfee6d65a11f7eace8fe2e908bebd4100858
Mor Tsadok <Mortmanage@gmail.com>
2019-12-07 20:54:04 +0200

    initial commit

--- client/src/components/Openingpage/Openingpage.js
+++ client/src/components/Openingpage/Openingpage.js
@@ -2,9 +2,12 @@ import React from "react";
 import "../Openingpage/Openingpage.css";
 import { Carousel } from "react-bootstrap";
 import Health from "../../img/Health.png";
-import Myprofile from "../../img/Myprofile.png";
 import Subscription from "../../img/Subscription.png";
 import TrainingPlan from "../../img/TrainingPlan.png";
+import UserSettings from "../../img/UserSettings.png";
+import Live from "../../img/Live.png";
+import Store from "../../img/Store.png";
+import Notifications from "../../img/Notifications.png";
 import { Link } from "react-router-dom";
 
 const Openingpage = () => {
@@ -18,40 +21,53 @@ const Openingpage = () => {
 const FirstPage = () => (
   <div>
     <h2> ברוכים הבאים לאפליקציית - Maxfit!</h2>
+
     <div className="welcome-text">
-      אפליקציית חדר הכושר תאפשר למתאמני חדר הכושר לבצע פעולות כמו: בניית תוכניות
-      אימונים, מעקב אחר סטטיסטיקות, לצפות בכמות המתאמנים בזמן אמת, חידוש מנוי,
-      הצהרת בריאות, חנות חדר הכושר, עדכונים ועוד.
+      אפליקציית חדר הכושר תאפשר למתאמנים לבצע פעולות כמו: בניית תוכניות אימונים,
+      מעקב אחר סטטיסטיקות, לצפות בכמות המתאמנים בזמן אמת, חידוש מנוי, הצהרת
+      בריאות, חנות חדר הכושר, עדכונים ועוד.
     </div>
+
     <div className="Carousel-content ">
       <Carousel>
         <Carousel.Item>
-          <img className="d-block w-100 " src={Health} alt="Maxfit Dashboard" />
+          <img className="d-block w-100" src={Health} alt="Health" />
         </Carousel.Item>
+
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={Myprofile}
-            alt="New Client page"
-          />
+          <img className="d-block w-100" src={Subscription} alt="Sub" />
         </Carousel.Item>
+
         <Carousel.Item>
-          <img className="d-block w-100" src={Subscription} alt="Third slide" />
+          <img className="d-block w-100" src={TrainingPlan} alt="Plan" />
         </Carousel.Item>
+
         <Carousel.Item>
-          <img className="d-block w-100" src={TrainingPlan} alt="Third slide" />
+          <img className="d-block w-100" src={UserSettings} alt="Settings" />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="d-block w-100" src={Live} alt="Live" />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="d-block w-100" src={Store} alt="Store" />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="d-block w-100" src={Notifications} alt="Notif" />
         </Carousel.Item>
       </Carousel>
     </div>
+
     <div className="Button-content ">
       <div className="MainPage-text">
-        נשמע מעניין?
         <Link to="/Register">
           <button>הירשם עכשיו</button>
         </Link>
       </div>
+
       <div className="MainPage-text">
-        כבר רשום?
         <Link to="/Login">
           <button>התחבר</button>
         </Link>
