fbf8ecc5e6c6a2f6d25f65427dcc515c806afc49
eden <eelmalich2@gmail.com>
2020-01-12 12:27:51 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const connectDB = require("./config/db");
+const connectDB = require('./config/db');
+const path = require('path');
 
 connectDB();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use(express.json({ extended: false }));
-app.use("/api/Nclient", require("./routes/api/Nclient"));
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/health", require("./routes/api/health"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/forgotPass", require("./routes/api/forgotPass"));
-app.use("/api/settingsForgotPass", require("./routes/api/settingsForgetPass"));
-app.use("/api/resetEmail", require("./routes/api/resetEmail"));
+app.use('/api/Nclient', require('./routes/api/Nclient'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/health', require('./routes/api/health'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/forgotPass', require('./routes/api/forgotPass'));
+app.use('/api/settingsForgotPass', require('./routes/api/settingsForgetPass'));
+app.use('/api/resetEmail', require('./routes/api/resetEmail'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
