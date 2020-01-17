b660c55a0f933326ab8d896d9bba54f9d127c2b5
eden <eelmalich2@gmail.com>
2020-01-17 14:00:07 +0200

    store Mobile

--- client/src/components/Store/Store.js
+++ client/src/components/Store/Store.js
@@ -1,209 +1,99 @@
-import React from "react";
-import { Carousel } from "react-bootstrap";
-import { Badge } from "react-bootstrap";
-import "./Store.css";
-import pic1 from "../../img/Bulk Muscle.png";
-import pic2 from "../../img/Dymatize Super Mass Gainer.png";
-import pic3 from "../../img/Extreme MASS.png";
-import pic4 from "../../img/MusclePharm Combat XL Mass Gainer.png";
-import pic5 from "../../img/Advance Anabolic - Massive Gainer.png";
-import pic6 from "../../img/NPL Elite Gainer.png";
-import pic7 from "../../img/Original MASS Gainer Protein.png";
-import pic8 from "../../img/Under Armour Drive Shoes.png";
-import pic9 from "../../img/Under Armour Running Shoes.png";
-import pic10 from "../../img/Asics Gel Resolution.png";
-import pic11 from "../../img/Nike Air.png";
-import pic12 from "../../img/Nike Athletic Shoes.png";
-import pic13 from "../../img/Nike Training Shoes.png";
-import pic14 from "../../img/Nike Metcon Black-Volt.png";
-import Navbar from "../Navbar/Navbar";
+import React, { Fragment } from 'react';
+import { Carousel } from 'react-bootstrap';
+import { Badge } from 'react-bootstrap';
+import './Store.css';
+import pic1 from '../../img/Bulk Muscle.png';
+import pic2 from '../../img/Dymatize Super Mass Gainer.png';
+import pic3 from '../../img/Extreme MASS.png';
+import pic4 from '../../img/MusclePharm Combat XL Mass Gainer.png';
+import pic5 from '../../img/Advance Anabolic - Massive Gainer.png';
+import pic6 from '../../img/NPL Elite Gainer.png';
+import pic7 from '../../img/Original MASS Gainer Protein.png';
+import pic8 from '../../img/Under Armour Drive Shoes.png';
+import pic9 from '../../img/Under Armour Running Shoes.png';
+import pic10 from '../../img/Asics Gel Resolution.png';
+import pic11 from '../../img/Nike Air.png';
+import pic12 from '../../img/Nike Athletic Shoes.png';
+import pic13 from '../../img/Nike Training Shoes.png';
+import pic14 from '../../img/Nike Metcon Black-Volt.png';
+import Navbar from '../Navbar/Navbar';
+import MobileNav from '../Mobile/MobileNav';
+import MediaQuery from 'react-responsive';
 
 const Store = () => {
+  const storeImages = [
+    { src: pic1, alt: 'BPI - BULK MUSCLE' },
+    { src: pic2, alt: 'Dymatize Super Mass' },
+    { src: pic3, alt: 'Extreme MASS' },
+    { src: pic4, alt: 'MusclePharm Combat XL Mass' },
+    { src: pic5, alt: 'Advance Anabolic - Massive' },
+    { src: pic6, alt: 'NPL Elite Gainer' },
+    { src: pic7, alt: 'Original MASS Gainer Protein' }
+  ];
+  const storeImages1 = [
+    { src: pic8, alt: 'Under Armour Drive Shoes' },
+    { src: pic9, alt: 'Asics Gel Resolution' },
+    { src: pic10, alt: 'Asics Gel Resolution' },
+    { src: pic11, alt: 'Nike Air' },
+    { src: pic12, alt: 'Nike Athletic Shoes' },
+    { src: pic13, alt: 'Nike Training Shoes' },
+    { src: pic14, alt: 'Nike Metcon Black - Volt' }
+  ];
+
   return (
-    <div className="Store">
-      <Navbar />
-      <BoxStore />
-    </div>
+    <Fragment>
+      <MediaQuery maxDeviceWidth={1024}>
+        <MobileNav />
+        <MobileStore storeImages={storeImages} storeImages1={storeImages1} />
+      </MediaQuery>
+      <MediaQuery minDeviceWidth={1280}>
+        <div className='Store'>
+          <Navbar />
+          <BoxStore storeImages={storeImages} storeImages1={storeImages1} />
+        </div>
+      </MediaQuery>
+    </Fragment>
   );
 };
 
-const BoxStore = props => (
-  <div className="boxStore">
-    <div className="Inside_BoxStore">
-      <p className="font-weight-light">חנות</p>
-      <div className="Flex-content ">
+const BoxStore = ({ storeImages, storeImages1 }) => (
+  <div className='boxStore'>
+    <div className='Inside_BoxStore'>
+      <p className='font-weight-light'>חנות</p>
+      <div className='Flex-content '>
         <Carousel>
-          <Carousel.Item>
-            <img className="d-block w-100" src={pic1} alt="BPI - BULK MUSCLE" />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                BPI - BULK MUSCLE
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={pic2}
-              alt="Dymatize Super Mass"
-            />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                Dymatize Super Mass
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img className="d-block w-100" src={pic3} alt="Extreme MASS" />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                Extreme MASS
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={pic4}
-              alt="MusclePharm Combat XL Mass"
-            />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                MusclePharm Combat XL Mass
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={pic5}
-              alt="Advance Anabolic - Massive"
-            />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                Advance Anabolic - Massive
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img className="d-block w-100" src={pic6} alt="NPL Elite Gainer" />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                NPL Elite Gainer
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={pic7}
-              alt="Original MASS Gainer Protein"
-            />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                Original MASS Gainer Protein
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
+          {storeImages.map((item, index) => (
+            <Carousel.Item key={index}>
+              <img className='d-block w-100' src={item.src} alt={item.alt} />
+              <Carousel.Caption>
+                <Badge pill variant='dark'>
+                  {item.alt}
+                </Badge>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
         </Carousel>
-
         <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={pic8}
-              alt="Under Armour Drive Shoes"
-            />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                Under Armour Drive Shoes
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={pic9}
-              alt="Under Armour Running Shoes"
-            />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                Under Armour Running Shoes
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={pic10}
-              alt="Asics Gel Resolution"
-            />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                Asics Gel Resolution
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img className="d-block w-100" src={pic11} alt="Nike Air" />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                Nike Air
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={pic12}
-              alt="Nike Athletic Shoes"
-            />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                Nike Athletic Shoes
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={pic13}
-              alt="Nike Training Shoes"
-            />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                Nike Training Shoes
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={pic14}
-              alt="Nike Metcon Black-Volt"
-            />
-            <Carousel.Caption>
-              <Badge pill variant="dark">
-                Nike Metcon Black - Volt
-              </Badge>
-            </Carousel.Caption>
-          </Carousel.Item>
+          {storeImages1.map((item, index) => (
+            <Carousel.Item key={index}>
+              <img className='d-block w-100' src={item.src} alt={item.alt} />
+              <Carousel.Caption>
+                <Badge pill variant='dark'>
+                  {item.alt}
+                </Badge>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
         </Carousel>
       </div>
     </div>
   </div>
 );
-
+const MobileStore = ({ storeImages, storeImages1 }) => (
+  <div className='Mobile'>
+    <main className='main'>
+      <BoxStore storeImages={storeImages} storeImages1={storeImages1} />
+    </main>
+  </div>
+);
 export default Store;
