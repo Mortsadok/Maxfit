8c9e327428a032685b41b1f81de6934d59019ac3
eden <eelmalich2@gmail.com>
2020-03-07 22:43:36 +0200

    removed console logs

--- client/src/actions/updateAction.js
+++ client/src/actions/updateAction.js
@@ -1,9 +1,9 @@
-import axios from "axios";
-import { GET_UPDATES } from "./typeActions";
-import { setAlert } from "./alertAction";
+import axios from 'axios';
+import { GET_UPDATES } from './typeActions';
+import { setAlert } from './alertAction';
 export const getUpdates = () => async dispatch => {
   try {
-    const res = await axios.get("/api/updates");
+    const res = await axios.get('/api/updates');
     dispatch({
       type: GET_UPDATES,
       payload: res.data
@@ -16,20 +16,19 @@ export const changeReadMessage = (_id, readMessage) => async dispatch => {
   try {
     const config = {
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       }
     };
     const body = JSON.stringify({ _id, readMessage });
-    await axios.post("/api/updates", body, config);
+    await axios.post('/api/updates', body, config);
   } catch (err) {
     console.error(err.message);
   }
 };
 export const deleteNotification = _id => async dispatch => {
-  console.log(_id);
   try {
     await axios.delete(`api/updates/${_id}`);
-    dispatch(setAlert("התראה נמחקה בהצלחה", "success"));
+    dispatch(setAlert('התראה נמחקה בהצלחה', 'success'));
   } catch (err) {
     console.error(err.message);
   }
