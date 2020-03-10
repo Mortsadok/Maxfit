deb47b28053dece9143c11e262813df70d436e51
Mor Tsadok <Mortmanage@gmail.com>
2020-03-10 23:02:51 +0200

    Change

--- client/src/components/Myprofile/UserSettings/PersonalDetails.js
+++ client/src/components/Myprofile/UserSettings/PersonalDetails.js
@@ -1,25 +1,25 @@
-import React, { Fragment } from 'react';
-import { Card } from 'react-bootstrap';
-import moment from 'moment';
+import React, { Fragment } from "react";
+import { Card } from "react-bootstrap";
+import moment from "moment";
 
 const PersonalDetails = ({ Name, email, Time, date }) => {
-  let getTime = moment(date).add('days', Time);
+  let getTime = moment(date).add("days", Time);
 
   let currentDate = moment();
-  let getUserTrainingTime = getTime.diff(currentDate, 'days');
+  let getUserTrainingTime = getTime.diff(currentDate, "days");
 
   return (
     <Fragment>
-      <Card border='secondary' style={{ width: '18rem' }}>
+      <Card border="secondary" style={{ width: "18rem" }}>
         <Card.Header>פרטים אישיים</Card.Header>
         <Card.Body>
-          <div className='SettingsHeadline'>שם מלא:</div>
-          <div className='SettingsInfo'>{Name}</div>
-          <div className='useLine'></div>
-          <div className='SettingsHeadline'>כתובת דואר אלקטרוני:</div>
-          <div className='SettingsInfo'>{email}</div>
-          <div className='useLine'></div>
-          <div className='SettingsHeadline'>מספר ימים לסיום המנוי:</div>
+          <div className="SettingsHeadline">שם מלא:</div>
+          <div className="SettingsInfo">{Name}</div>
+          <div className="useLine"></div>
+          <div className="SettingsHeadline">כתובת דואר אלקטרוני:</div>
+          <div className="SettingsInfo">{email}</div>
+          <div className="useLine"></div>
+          <div className="SettingsHeadline">מספר ימים לסיום המנוי:</div>
           <h4>{getUserTrainingTime}</h4>
         </Card.Body>
       </Card>
